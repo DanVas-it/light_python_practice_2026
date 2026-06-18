4f687e1da6f7445a019e227a2ade44cdeb588a63
DanVas <dany123098123@yandex.com>
2026-06-18 10:42:17 +0700

    Переписан сканер без использования облегчающей функции rglib

--- src/scanner.py
+++ src/scanner.py
@@ -1,24 +1,39 @@
-from pathlib import Path
+import os
 from datetime import datetime
+from pathlib import Path
+
 
 def scan_directory(target_dir: Path):
-
     files_metadata = []
 
-    # Метод rglob('*') рекурсивно ищет все файлы и папки
-    for path in target_dir.rglob('*'):
-        if path.is_file():
-            stat = path.stat()
+    base_path = str(target_dir)
 
-            size_bytes = stat.st_size
+    def traverse(current_path: str):
+        try:
+            items = os.listdir(current_path)
+        except PermissionError:
+            return
 
-            mod_time = datetime.fromtimestamp(stat.st_mtime).strftime('%Y-%m-%d %H:%M:%S')
+        for item in items:
+            full_path = os.path.join(current_path, item)
 
-            # Сохраняем данные
-            files_metadata.append({
-                'path': str(path),
-                'size': size_bytes,
-                'mtime': mod_time
-            })
+            if os.path.isdir(full_path):
+                traverse(full_path)
+
+            elif os.path.isfile(full_path):
+                try:
+                    stat = os.stat(full_path)
+                    size_bytes = stat.st_size
+                    mod_time = datetime.fromtimestamp(stat.st_mtime).strftime('%Y-%m-%d %H:%M:%S')
+
+                    files_metadata.append({
+                        'path': full_path,
+                        'size': size_bytes,
+                        'mtime': mod_time
+                    })
+                except (PermissionError, OSError):
+                    continue
+
+    traverse(base_path)
 
     return files_metadata
