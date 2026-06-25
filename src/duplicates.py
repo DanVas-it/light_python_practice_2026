dd3073fe4f6d65f1d333607f813c047f342e0c58
DanVas <dany123098123@yandex.com>
2026-06-25 17:37:07 +0700

    Изменение: Хеш берется из метаданных для поиска дубликатов

--- src/duplicates.py
+++ src/duplicates.py
@@ -14,20 +14,14 @@ def calculate_file_hash(filepath: str, chunk_size: int = 4096):
 
 
 def find_duplicates(files_metadata: list):
-
     hashes_map = {}
 
     for file_info in files_metadata:
-        file_hash = calculate_file_hash(file_info['path'])
-        if file_hash:
-            if file_hash not in hashes_map:
-                hashes_map[file_hash] = []
+        f_hash = file_info.get('hash')
+        if f_hash:
+            if f_hash not in hashes_map:
+                hashes_map[f_hash] = []
+            hashes_map[f_hash].append(file_info['path'])
 
-            hashes_map[file_hash].append(file_info['path'])
-
-    duplicates = {}
-    for file_hash, paths in hashes_map.items():
-        if len(paths) > 1:
-            duplicates[file_hash] = paths
-
+    duplicates = {h: paths for h, paths in hashes_map.items() if len(paths) > 1}
     return duplicates
